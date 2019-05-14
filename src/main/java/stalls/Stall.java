94594f732ac90b7fb71cb922fa97472995673643
sierratango34 <k.sritawan@gmail.com>
2019-05-14 17:44:26 +0100

    TobaccoStallTest: getMinAge test pass

--- src/main/java/stalls/Stall.java
+++ src/main/java/stalls/Stall.java
@@ -1,4 +1,32 @@
 package stalls;
 
 public abstract class Stall {
+
+    protected String name;
+    protected String ownerName;
+    protected int parkingSpot;
+    protected int rating;
+
+    public Stall(String name, String ownerName, int parkingSpot, int rating) {
+        this.name = name;
+        this.ownerName = ownerName;
+        this.parkingSpot = parkingSpot;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public int getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public int getRating() {
+        return rating;
+    }
 }
--- src/main/java/stalls/TobaccoStall.java
+++ src/main/java/stalls/TobaccoStall.java
@@ -1,4 +1,15 @@
 package stalls;
 
-public class TobaccoStall {
+public class TobaccoStall extends Stall{
+
+    private int minAge;
+
+    public TobaccoStall(String name, String ownerName, int parkingSpot, int rating) {
+        super(name, ownerName, parkingSpot, rating);
+        this.minAge = 18;
+    }
+
+    public int getMinAge() {
+        return minAge;
+    }
 }
